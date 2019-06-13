7fb793c07abfc750c4851fdcaa7cafec86be5296
Geoff Smith <smith260194@gmail.com>
2019-06-13 23:28:19 +0100

    Using config to store apikey

--- jasmine/jasmine-standalone-3.4.0/src/Thermostat.js
+++ jasmine/jasmine-standalone-3.4.0/src/Thermostat.js
@@ -16,7 +16,7 @@ Thermostat.prototype.temperatureFormatted = function () {
 };
 
 Thermostat.prototype.APItemperature = function () {
-  $.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4c1a3536febad70a64e3f920a03f372a&units=metric", function(data) {
+  $.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=" + weatherApiKey + "&units=metric", function(data) {
     response = data;
     console.log(response.weather[0]['main'])
     var icon = response.weather[0]['icon']
